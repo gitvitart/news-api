45227a531ad1318d2454e31a065edb887f080830
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-01-24 21:05:13 +0300

    integration

---
+++ NewsAPI/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace NewsAPI
+{
+    public class DataContext(string connectionString) : DbContext
+    {
+        private readonly string ConnectionString = connectionString;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- NewsAPI/Program.cs
+++ NewsAPI/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//- DB_HOST = postgres
+//- POSTGRES_DB = newsdb
+//- POSTGRES_USER = postgres
+//- POSTGRES_PASSWORD = 123Secret_a
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var dbPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+var connectionString = $"Server = {dbHost};Port=5432;Database={dbName};User Id ={dbUser};Password={dbPassword}";
+builder.Services.AddScoped(provider => new DataContext(connectionString));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
